6d71c3669daa2cae2c5e322da0ed5309900a14d2
kdrbipul <kdrbipul@gmail.com>
2023-08-17 09:29:59 +0600

    first commit

--- object-creat.js
+++ object-creat.js
@@ -1,21 +1,34 @@
-const student={
-    name: "Abdul Kader",
-    job: "web developer"
+// const student={
+//     name: "Abdul Kader",
+//     job: "web developer"
+// }
+
+// // console.log(student);
+
+
+// const person = new Object();
+// person.name = "Abdul Kader";
+// // console.log(person);
+
+
+// // const human = Object.create(null);
+// // human.name ="Abdul Kader";
+// // console.log("Abdul Kader");
+
+
+
+// // const human = Object.create(student);
+// // console.log(human.name);
+
+
+class People {
+    constructor(name, age){
+        this.name=name;
+        this.age =age;
+    }
 }
 
-// console.log(student);
-
-
-const person = new Object();
-person.name = "Abdul Kader";
-// console.log(person);
-
-
-// const human = Object.create(null);
-// human.name ="Abdul Kader";
-// console.log("Abdul Kader");
-
-
-
-const human = Object.create(student);
-console.log(human.name);
+const people1 = new People("Abdul kader", 23);
+const people2 = new People("Mst Mithila", 18);
+console.log(people1 );
+console.log(people2 );

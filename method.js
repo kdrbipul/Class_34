b189aa6bd5f2dc11db17d653bd60478bba45ca28
kdrbipul <kdrbipul@gmail.com>
2023-08-18 15:26:28 +0600

    first commit

--- method.js
+++ method.js
@@ -16,9 +16,16 @@ const student = {
         console.log(this.name, "Taking Exam");
     },
     giveTreat:function(expense, tips){
-        this.money=this.money - expense;
+        this.money=this.money - expense - tips;
         return this.money;
     }
 }
 
-student.takeExam();
+// student.takeExam();
+
+
+const reamining=student.giveTreat(700, 50);
+console.log(reamining);
+
+const remaining2=student.giveTreat(500, 100);
+console.log(remaining2);

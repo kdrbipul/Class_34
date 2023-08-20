8151f5dac29f933dbf017ad1cbc6832ee08ecc65
kdrbipul <kdrbipul@gmail.com>
2023-08-20 09:52:20 +0600

    first commit

---
+++ properties.js
@@ -0,0 +1,13 @@
+const bottle ={
+    color:'Green',
+    hold:'Water',
+    price:50,
+    inCleaned:true,
+}
+
+const bottleKeys = Object.keys(bottle);
+console.log(bottleKeys);
+
+const bottleValues = Object.values(bottle);
+console.log(bottleValues);
+
